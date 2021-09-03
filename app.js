f85cfc72e01cce1d17e2c1f91adec1046f58c98e
lingchen <lingchen@migu.cn>
2021-09-03 11:32:23 +0800

    feat: update

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+
+// 引入express-async-error用来处理全局异常
 require('express-async-errors')
 const app = express()
 
+// 配置解析请求：application/json
 app.use(express.json())
+// 配置解析请求：application/x-www-form-urlencoded
+app.use(express.urlencoded())
 
-const { getDb } = require('./db')
+const { getDb, saveDb } = require('./db')
 
-app.get('/todos', async (req, res, next) => {
+// 查询全部列表
+app.get('/todos', async (req, res) => {
     const { todos } = await getDb()
     res.status(200).json({
         code: 200,
@@ -14,7 +20,8 @@ app.get('/todos', async (req, res, next) => {
     })
 })
 
-app.get('/todos/:id', async (req, res, next) => {
+// 查询某个id的数据
+app.get('/todos/:id', async (req, res) => {
     const { id } = req.params
     const { todos } = await getDb()
     const todo = todos.find(todo => String(todo.id) === id) || null
@@ -24,22 +31,77 @@ app.get('/todos/:id', async (req, res, next) => {
     })
 })
 
-app.post('/todos', (req, res) => {
-    const data = req.body
-    console.log(data)
-    // const { todos } = await getDb()
+// 创建数据
+app.post('/todos', async (req, res) => {
+    // 1.获取客户端提交的参数
+    const todo = req.body
+    // 2.数据验证
+    if (!todo.title) {
+        return res.status(422).json({
+            code: 422,
+            msg: 'The field title is required'
+        })
+    }
+    // 3.数据验证通过，存储到db中
+    const db = await getDb()
+    const lastTodo = db.todos[db.todos.length - 1]
+    const currentId = lastTodo ? lastTodo.id + 1 : 1
+    todo.id = currentId
+    db.todos.push(todo)
+    await saveDb(db)
+    // 4.发送响应
+    res.status(201).json({
+        code: 201,
+        msg: 'create success',
+        data: todo
+    })
 })
 
-app.patch('/todos/:id', (req, res) => {
+// 修改某个id的数据
+app.patch('/todos/:id', async (req, res) => {
+    // 1.获取表单数据
+    const todo = req.body
+    // 2.查找item
     const { id } = req.params
-    res.send('patch todos')
+    const db = await getDb()
+    const result = db.todos.find(todo => String(todo.id) === id)
+    if (!result) {
+        return res.status(404).json({
+            code: 404,
+            msg: 'Not find this id'
+        })
+    }
+    // 合并item，并存储
+    Object.assign(result, todo)
+    await saveDb(db)
+    // 4.发送响应
+    res.status(200).json({
+        code: 200,
+        msg: 'edit success',
+        data: result
+    })
 })
 
-app.delete('/todos/:id', (req, res) => {
+// 删除某个id的数据
+app.delete('/todos/:id', async (req, res) => {
     const { id } = req.params
-    res.send('delete todos')
+    const db = await getDb()
+    const index = db.todos.findIndex(todo => String(todo.id) === id)
+    if (index === -1) {
+        return res.status(404).json({
+            code: 404,
+            msg: 'Not find this id'
+        })
+    }
+    db.todos.splice(index, 1)
+    await saveDb(db)
+    res.status(200).json({
+        code: 200,
+        msg: 'delete success'
+    })
 })
 
+// 全局处理async的异常
 app.use((err, req, res, next) => {
     if (err) {
         res.json({
@@ -51,5 +113,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(3000, () => {
-    console.log('服务已启动 http://localhost:3000/')
+    console.log('Server is running at http://localhost:3000/')
 })
--- db.js
+++ db.js
@@ -3,10 +3,17 @@ const path = require('path')
 const { promisify } = require('util')
 
 const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
 const dbPath = path.join(__dirname, './db.json')
 const encoded = 'utf8'
 
-exports.getDb = async (next) => {
+exports.getDb = async () => {
     const data = await readFile(dbPath, encoded)
     return JSON.parse(data)
+}
+
+exports.saveDb = async db => {
+    const data = JSON.stringify(db, null, '    ') // 缩进4个空格
+    await writeFile(dbPath, data)
 }
