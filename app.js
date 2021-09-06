9693c03becd815e607acbea61fd55863c6dc0e5e
lingchen <lingchen@migu.cn>
2021-09-06 17:54:58 +0800

    feat: 404统一处理

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = require('./router')
+const morgan = require('morgan')
 const app = express()
 
 // 配置解析请求：application/json
@@ -7,11 +8,12 @@ app.use(express.json())
 // 配置解析请求：application/x-www-form-urlencoded
 app.use(express.urlencoded())
 
-// 中间件，打印日志
-app.use((req, res, next) => {
-    console.log(req.method, req.url, Date.now())
-    next()
-})
+// 中间件morgan，打印日志
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+// app.use((req, res, next) => {
+//     console.log(req.method, req.url, Date.now())
+//     next()
+// })
 
 // 挂载路由
 app.use(router)
@@ -19,6 +21,14 @@ app.use(router)
 // 挂载路由限定访问前缀
 // app.use('/abc', router)
 
-app.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000/')
+// 在所有中间件之后处理404，顺序很重要
+app.use((req, res, next) => {
+    res.status(404).json({
+        code: 404,
+        msg: '404 not found'
+    })
+})
+
+app.listen(3300, () => {
+    console.log('Server is running at http://localhost:3300/')
 })
